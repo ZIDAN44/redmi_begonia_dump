0bc512a125a1af1ae5c95b8f95a94f9ee56627a1
Zinadin Zidan <zidan.roking@gmail.com>
2022-02-15 19:32:26 +0100

    Add extras for begonia-user 11 RP1A.200720.011 V12.5.7.0.RGGMIXM release-keys
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## begonia-user 11 RP1A.200720.011 V12.5.7.0.RGGMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6785
+- Codename: begonia
+- Brand: Redmi
+- Flavor: begonia-user
+- Release Version: 11
+- Id: RP1A.200720.011
+- Incremental: V12.5.7.0.RGGMIXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.7.0.RGGMIXM:user/release-keys
+- OTA version: 
+- Branch: begonia-user-11-RP1A.200720.011-V12.5.7.0.RGGMIXM-release-keys
+- Repo: redmi_begonia_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/product/app/Messages/Messages.apk.* 2>/dev/null >> system/system/product/app/Messages/Messages.apk
+rm -f system/system/product/app/Messages/Messages.apk.* 2>/dev/null
+cat system/system/product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> system/system/product/app/Gmail2/Gmail2.apk
+rm -f system/system/product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/product/app/YouTube/YouTube.apk
+rm -f system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/product/app/Maps/Maps.apk.* 2>/dev/null >> system/system/product/app/Maps/Maps.apk
+rm -f system/system/product/app/Maps/Maps.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat vendor/data-app/MiBrowserGlobalVendor/MiBrowserGlobalVendor.apk.* 2>/dev/null >> vendor/data-app/MiBrowserGlobalVendor/MiBrowserGlobalVendor.apk
+rm -f vendor/data-app/MiBrowserGlobalVendor/MiBrowserGlobalVendor.apk.* 2>/dev/null
+cat vendor/data-app/Photos/Photos.apk.* 2>/dev/null >> vendor/data-app/Photos/Photos.apk
+rm -f vendor/data-app/Photos/Photos.apk.* 2>/dev/null
+cat vendor/etc/camera/apu_miai_fragment.bin.* 2>/dev/null >> vendor/etc/camera/apu_miai_fragment.bin
+rm -f vendor/etc/camera/apu_miai_fragment.bin.* 2>/dev/null
